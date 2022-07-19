a2389a587a2153c68c9e9429dca439502b0a9a7d
Ondřej Černý <ondrej.cerny@yieldigo.com>
2022-07-19 16:05:33 +0200

    banner parallax, simplify css

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
-import logo from './logo.svg';
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+import { Parallax, ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
 import { MessageSquare } from 'react-feather';
 import './App.css';
 
-function App() {
-  const color = "#12c12c";
+import header from './img/placeholder-header.jpg';
 
+function App() {
   return (
     <div className="app">
       <ParallaxProvider>
         <header className="app-header">
-          <Parallax speed={-10}>
-            <img src='https://picsum.photos/600/200?grayscale' alt='placeholder' />
-          </Parallax>
+          <ParallaxBanner
+            className='test' style={{ height: "80vh" }}
+            layers={[
+              { image: header, speed: -20 },
+              {
+                children: (<h1>Header text</h1>),
+                speed: -15,
+                expanded: false
+            }]} />
         </header>
-        <nav className='app-nav'>
-          <a href="">Some</a>
-          <a href="">Nav</a>
-          <a href="">Could</a>
-          <a href="">Be</a>
-          <a href="">Here</a>
-        </nav>
-<section>
-        <a className='app-comment-float' href="#app-comment-section">
-          <MessageSquare size='2em' />
-        </a>
-        </section>
-        <section className='app-content-section'>
-          Content
-        </section>
-        <section id='app-comment-section'>
-          Here I am
-        </section>
-        <footer className='app-footer'>
-          <a
-            className="app-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </footer>
       </ParallaxProvider>
+      <nav className='app-nav'>
+        <a href="">Some</a>
+        <a href="">Nav</a>
+        <a href="">Could</a>
+        <a href="">Be</a>
+        <a href="">Here</a>
+      </nav>
+      <a className='app-comment-float' href="#app-comment-section">
+        <MessageSquare size='2em' />
+      </a>
+      <section className='app-content-section'>
+        Content
+      </section>
+      <section className='app-comment-section' id='app-comment-section'>
+        Comments
+      </section>
+      <footer className='app-footer'>
+        Foot
+      </footer>
+
     </div>
   );
 }
