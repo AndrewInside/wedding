c8da75d16cdf80ddc82bf39c51ab34c61b9301f6
AndrewInside <its.andrew.black@gmail.com>
2022-07-25 21:51:15 +0200

    Test

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ export const Header = () => {
             layers={[
               { image: header, speed: -50, expanded: false, opacity: [1.6, 0.4] },
               {
-                children: (<h1 style={{lineHeight:"100vh"}}>Header text</h1>),
+                children: (<h1 style={{lineHeight:"100vh"}}>Header test text</h1>),
                 speed: -30,
                 expanded: false
             }]} />
