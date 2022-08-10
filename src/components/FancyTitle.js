36f53d22f8e8b622ba0d2c840db48e0ac7816f9f
Ondřej Černý <ondrej.cerny@yieldigo.com>
2022-08-10 08:40:59 +0200

    fancy title

--- src/components/FancyTitle.js
+++ src/components/FancyTitle.js
@@ -4,7 +4,9 @@ const FancyTitle = props => {
     const { children } = props;
     return (
         <div className="fancy">
+            <span></span>
             <h2>{children}</h2>
+            <span></span>
         </div>
     )
 };
