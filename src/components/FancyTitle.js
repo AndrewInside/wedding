6f349a60d423f3daa563d00fd39abe6cc2eaa2f8
Ondřej Černý <ondrej.cerny@yieldigo.com>
2022-08-26 13:39:46 +0200

    die

--- src/components/FancyTitle.js
+++ src/components/FancyTitle.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import Die from '../img/die.svg';
 
 const FancyTitle = props => {
     const { children } = props;
     return (
         <div className="fancy">
-            <span></span>
+            <img src={Die} alt="die-logo" />
             <h2>{children}</h2>
-            <span></span>
         </div>
     )
 };
