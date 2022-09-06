ffdadb4e941e7d493781ef1a86437ce6a7120de9
Ondřej Černý <ondrej.cerny@yieldigo.com>
2022-09-06 18:40:06 +0200

    test end

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <ParallaxProvider>
       <header className="app-header wide">
-        <ParallaxBanner style={{ height: "calc(100vh - calc(100vh - 100%))" }}
+        <ParallaxBanner style={{ height: "100vh" }}
           layers={[
             { image: header, speed: -50, expanded: false, opacity: [1.6, 0.4]},
             {
