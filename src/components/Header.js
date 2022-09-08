f447e7bebe053fa0692ee25f60320e13384f9e48
Ondřej Černý <ondrej.cerny@yieldigo.com>
2022-09-08 19:22:20 +0200

    header rework

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
-import header from '../img/header_centered.jpg';
-import DownButton from './DownButton';
+import React from "react";
+import header from "../img/header_centered.jpg";
+import DownButton from "./DownButton";
 
 export const Header = () => {
   return (
-    <ParallaxProvider>
-      <header className="app-header wide">
-        <ParallaxBanner style={{ height: "100vh" }}
+    <header className="app-header wide" style={{ backgroundImage: `url(${header})` }}>
+      <div className="app-header-text">
+        <div className="">
+          <h2>Bereme se.</h2>
+          <h2>Buď u&nbsp;toho.</h2>
+          <DownButton link="#nav-where" />
+        </div>
+      </div>
+      {/* <ParallaxBanner style={{ height: "100vh" }}
           layers={[
-            { image: header, speed: -50, expanded: false, opacity: [1.6, 0.4]},
+            { image: header, height: "50%", speed: -50, expanded: false, opacity: [1.6, 0.4]},
             {
               children: (
                 <div className="app-header-wrapper">
@@ -21,8 +26,7 @@ export const Header = () => {
               speed: 10,
               expanded: false
             },
-          ]} />
-      </header>
-    </ParallaxProvider>
+          ]} /> */}
+    </header>
   )
 }
